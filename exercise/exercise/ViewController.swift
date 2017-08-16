6088500034592a6eb7c720d058362096c47daea9
rossthetruth22 <31022402+rossthetruth22@users.noreply.github.com>
2017-08-16 17:47:53 -0400

    Initial commit
    Trying this excercise

---
+++ exercise/exercise/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  exercise
+//
+//  Created by Royce Reynolds on 8/16/17.
+//  Copyright © 2017 Royce Reynolds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //adding a comment for exercise
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
